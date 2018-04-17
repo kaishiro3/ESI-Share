f58c9de085657d5fd58efb31398952780b546ada
MiguelangelX72 <38109568+MiguelangelX72@users.noreply.github.com>
2018-04-17 17:42:04 +0200

    Update usuarios.h

--- usuarios.h
+++ usuarios.h
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <usuarios.c>
 
+void login();                   //Procedimiento que Mostrara pantalla de login y comprobara que existe.
+int check_user(char *, char *); //Funcion que comprobara si el usuario existe y devolvera entero.
+int mostrar();                  //Funcion que mostrara el menu de usuario, y devuelve entero con la elección.
+
+//Precondicion: m_usuarios y modif inicializados, en lon la longitud de m_usuarios y -1<indice<lon
+/*Descripcion: Reemplaza m_usuarios[indice] con modif*/
+void modificar_usuario(usuarios *,int,int,usuarios);
 
 int main(){
 

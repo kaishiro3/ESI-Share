bdf12139ec3130d0cd150abb835bdfa0d64c4aad
MiguelangelX72 <38109568+MiguelangelX72@users.noreply.github.com>
2018-04-18 17:48:49 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include"ficheros.c"
+#include"vehiculos.c"
+#include"usuarios.c"
+#include"viajes.c"
+
+main(){
+    vehiculos *x;
+	FILE *fich;
+	int n,i,id,oper;
+	n=0;
+    cargar_fich_vehiculos(&x,&n);
+		do
+		{
+			printf("ID de Usuario: ");
+			scanf("%i",&id);
+		}while(id<1);
+		oper=mostrar_vehiculos_de_usuario(x,n,id);
+		if(oper!=-1)
+		{
+			if(oper=-2)
+			{
+				agregar_vehiculo(&x,&n,id);
+			}
+		}
+	guardar_fich_vehiculos(x,n);
+}

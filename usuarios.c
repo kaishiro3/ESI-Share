b6c4b0dd5257a68e55d38aeeffac392e5fe60a13
MiguelangelX72 <38109568+MiguelangelX72@users.noreply.github.com>
2018-04-18 23:18:12 +0200

    Update usuarios.c

--- usuarios.c
+++ usuarios.c
@@ -7,19 +7,15 @@ void modificar_usuario(usuarios *,int,int,usuarios);
 
 
 /*int main(){
-
 mostrar_lista_usuarios(&d_usuarios,&longitud);
 //login;
-
 switch (tipo_usuario){
-
     case 0:
     resp=mostrar_menu_usuario();
     break;
     case 1:
     resp=mostrar_menu_admin();
     break;
-
     }
 switch (resp){  //Switch que comprobara la seleccion del menu y que comprueba tipo de usuario para diferenciar entre una seleccion y otra.
 case 1:
@@ -27,50 +23,51 @@ case 1:
     //if (tipo_usuario==2) mostrar_lista_usuarios; //Muestra lista de todos usuarios.
     break;
 case 2:
-
     break;
 case 3:
     break;
 case 4:
     break;
 }
-
 return 0;
 }*/
 
 
 void login(){                   //Funcion que comprueba en usuarios.txt usuario y contraseña
+int admin;
 
 char usuario[15];
 char pass[15];
+	system("cls");
+	printf("\n\t Inicio de sesion\n\n");
     puts("Introduce usuario");
     scanf("%s",&usuario);
     puts("Introduce contraseña");
     scanf("%s",&pass);
-usuario_actual=check_user(usuario,pass,&l_usuarios); //Comprueba si existe usuario y devuelve un entero con el indice de los datos del usuario o -1 si no existe.
-if (tipo_usuario==-1) {
+if (check_user(usuario,pass,&admin)==-1) { //Comprueba si existe usuario y devuelve un entero con el indice de los datos del usuario o -1 si no existe.
 do{
-    puts("No existe usuario. Intentelo de nuevo.");
+    system("cls");
+    printf("\n\t Inicio de sesion\n\n");
+	puts("No existe usuario. Intentelo de nuevo.\n");
     puts("Introduce usuario");
     scanf("%s",&usuario);
     puts("Introduce contraseña");
     scanf("%s",&pass);
-    usuario_actual=check_user(usuario,pass,&l_usuarios);
-}while(tipo_usuario==-1);
+}while(check_user(usuario,pass,&admin)==-1);
 }
+if(admin==0) puts("Usuario normal");
+else puts("Administrador");
 }
 
-int check_user(char *usu,char *pass, int * lon){
+int check_user(char *usu,char *pass, int *admin){
 int i;
-        for(i=0;i<*lon;i++)
+*admin=0;
+        for(i=0;i<l_usuarios;i++)
         {
                 if(strcmp(m_usuarios[i].user,usu)==0 && strcmp(m_usuarios[i].password,pass)==0){
-                    tipo_usuario=m_usuarios[i].perfil;
-                    if(m_usuarios[i].estado==0) {
-                            puts("El usuario esta bloqueado. Contacte con el administrador.");
-                            system("pause");
-                            exit(0);
-                    }else return i;
+                tipo_usuario=m_usuarios[i].perfil;
+                *admin=m_usuarios[i].perfil;
+                return i;
                 }
         }
 return -1;
@@ -108,11 +105,10 @@ void mostrar_lista_usuarios(usuarios *m_usuarios,int *lon){
 int i;
 
 for(i=0;i<*lon;i++){
-    printf("Id: %d \n Nombre: %s \n Poblacion: %s \n Perfil: %d \n Usuario: %s \n Contraseña: %s \n Estado: %d",m_usuarios[i].id_Usuario,m_usuarios[i].nombre,m_usuarios[i].poblacion,m_usuarios[i].perfil,m_usuarios[i].user,m_usuarios[i].password,m_usuarios[i].estado);
+    printf("Id: %d \n Nombre: %s \n Poblacion: %s \n Perfil: %d \n Usuario: %s \n Contraseña: %s \n Estado: %d\n",m_usuarios[i].id_Usuario,m_usuarios[i].nombre,m_usuarios[i].poblacion,m_usuarios[i].perfil,m_usuarios[i].user,m_usuarios[i].password,m_usuarios[i].estado);
 }
 }
 
 void mostrar_datos_usuario(usuarios *m_usuarios,int *indice){
 printf("Id: %d \n Nombre: %s \n Poblacion: %s \n Perfil: %d \n Usuario: %s \n Contraseña: %s \n Estado: %d",m_usuarios[*indice].id_Usuario,m_usuarios[*indice].nombre,m_usuarios[*indice].poblacion,m_usuarios[*indice].perfil,m_usuarios[*indice].user,m_usuarios[*indice].password,m_usuarios[*indice].estado);
 }
-

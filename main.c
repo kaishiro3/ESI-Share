d1ead576d7309081990db2a94cd14422eb2aba27
MiguelangelX72 <38109568+MiguelangelX72@users.noreply.github.com>
2018-04-18 18:57:42 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,30 @@
+
 #include"ficheros.c"
 #include"vehiculos.c"
 #include"usuarios.c"
 #include"viajes.c"
 
 main(){
- /*   vehiculos *x;
-	FILE *fich;
 	int n,i,id,oper;
 	n=0;
-    cargar_fich_vehiculos(&x,&n);
+    cargar_ficheros();
 		do
 		{
 			printf("ID de Usuario: ");
 			scanf("%i",&id);
 		}while(id<1);
-		oper=mostrar_vehiculos_de_usuario(x,n,id);
+		for(i=0;i<l_usuarios;i++)
+		{
+			//if(m_usuarios[i].id_Usuario==id)
+			printf("%s ",m_usuarios[i].nombre);
+		}
+		oper=mostrar_vehiculos_de_usuario(id);
 		if(oper!=-1)
 		{
 			if(oper=-2)
 			{
-				agregar_vehiculo(&x,&n,id);
+				agregar_vehiculo(&m_vehiculos,&l_vehiculos,id);
 			}
 		}
-	guardar_fich_vehiculos(x,n);
-}*/
+	guardar_fich_vehiculos(m_vehiculos,l_vehiculos);
+}

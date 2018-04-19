accb2bfa0e197c9fd7eb0ffa0bb8a8388cd32319
jmorenop94 <38109479+jmorenop94@users.noreply.github.com>
2018-04-19 19:20:04 +0200

    Update incidencias.c

--- incidencias.c
+++ incidencias.c
@@ -34,9 +34,10 @@ void menu_inciadmin(){
     printf("4. Listar incidencias: \n");
     printf("5. Validar incidencias: \n");
     printf("6. Bloquear usuarios: \n");
+    printf("7. Volver.\n")
     printf("Introduce un numero para elegir una opcion: \n");
     scanf("%i", &resp);
- }while(resp<1 || resp>6);
+ }while(resp<1 || resp>7);
 switch(resp){
 case 1:
     crear_inciden();
@@ -59,7 +60,7 @@ case 6:
 }
 
 }
-}
+
 
 void modif_inciden(incidencias *m_incidencias, int lon, int i, incidencias modif){
 
@@ -73,7 +74,32 @@ void list_inciden(int *lon){
     for(i=0;i<*lon;i++){
         printf("Id del viaje: %d \n Id del usuario que registra: %d \n Id del usuario sobre el que recae: %d \n Descripcion: %s \n Estado: %s \n",m_incidencias[i].id_viaje,m_incidencias[i].id_us_registra,m_incidencias[i].id_us_incidencia,m_incidencias[i].desc_incidencia,m_incidencias[i].est_incidencia);
 }
-}
 
+void consult_inciden(int id){
+     int i,j,cursor;
+     i=j=0;
+     puts("");
+     while(i<l_incidencias && m_incidencias[i].id_us_incidencia!=id) i++;
+     while(i<l_incidencias && m_incidencias[i].id_us_incidencia==id)
+     {
+          j++;
+          printf("%i.- %s\n",j,m_incidencias[i].desc_incidencia);
+          i++;
+     }
+     if(j==0) puts("No tiene ninguna incidencia a su nombre");
+     printf("\n%i.- A%cadir\n%i.- Salir\n\n",j+1,164,j+2);
+     do
+     {
+           printf("Introduzca operacion: ");
+           scanf("%i",&cursor);
+     }while(cursor<1 || cursor>j+2);
+
+     if(cursor==j+1) cursor=-2;
+     else
+     {
+           if(cursor==j+2) cursor=-1;
+           else cursor=i-j+cursor;
+     }
+    return cursor;
 
 

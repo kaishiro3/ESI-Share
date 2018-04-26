9bf787c26953524505256a28a5d48a4bd40afc6d
manolito21 <33462344+manolito21@users.noreply.github.com>
2018-04-26 17:42:39 +0200

    Update usuarios.h

--- usuarios.h
+++ usuarios.h
@@ -5,12 +5,14 @@
 #include<stdlib.h>
 #endif // _USUARIOS_
 
+void modificar_usuario(int);
 int tipo_usuario,usuario_actual;               //Muestra el tipo de usuario que es. 1=usuario, 2=admin.
 void login();                   //Procedimiento que Mostrara pantalla de login y comprobara tipo usuario.
 int check_user(char *, char *,int *); //Funcion que comprobara si el usuario existe.
 int mostrar_menu_usuario();     //Funcion que mostrara el menu de usuario, y devuelve entero con la elección.
 int mostrar_menu_admin();       //Idem.
-void registrar_usuario();
+int registrar_usuario(usuarios **,int *);
 void mostrar_lista_usuarios(usuarios *,int *);
 void mostrar_datos_usuario(usuarios *,int *);
-int buscar_usuario_id(int id);
+int buscar_usuario_id(int);
+int buscar_usuario_user(char *);

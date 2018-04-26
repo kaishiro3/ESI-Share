f5978e0d7bb45fe36b7262b5387f51b4c48122b4
manolito21 <33462344+manolito21@users.noreply.github.com>
2018-04-26 18:51:50 +0200

    Update incidencias.h

--- incidencias.h
+++ incidencias.h
@@ -6,8 +6,8 @@
 void crear_inciden(incidencias **,viajes **,int *, int);
 void consult_inciden(incidencias *,int,int);
 void elimin_inciden(incidencias **, int *, int);
-void modif_inciden(incidencias *m_incidencias, int lon, int i, incidencias modif);
-void list_inciden(int *lon);
+void modif_inciden(incidencias *, int , int , incidencias);
+void list_inciden(int *);
 void valid_inciden(incidencias *);
 void bloquear_usu(usuarios *);
 void menu_inciusu();

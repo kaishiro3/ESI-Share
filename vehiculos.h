776a82f5984471403a3af8e4e3e23bb8fa2643b4
MiguelangelX72 <38109568+MiguelangelX72@users.noreply.github.com>
2018-04-30 07:20:53 +0200

    Update vehiculos.h

--- vehiculos.h
+++ vehiculos.h
@@ -6,28 +6,26 @@
 
 #endif
 
-//Descripcion: Imprime en pantalla cada uno de los datos de m_vehiculos
+//Postcondicion: Imprime en pantalla cada uno de los datos de m_vehiculos.
 void mostrar_lista_vehiculos();
 
-//Precondicion: id inicializado
-/*Descripcion: Imprime en pantalla un menu con los vehiculos pertenecientes a id.
-Devuelve el indice del vehiculo elegido en m_vehiculos, -2 si desea agregar un vehiculo y -1 si desea salir */
-void menu_usuario_vehiculos(int id);
+//Precondicion: id_Usuario > 0.
+//Postcondicion: Accede al menu de vehiculos de usuarios estandar mostrando los vehiculos pertenecientes a id.
+void menu_usuario_vehiculos(int id_Usuario);
 
-//Precondicion: m_vehiculos y id inicializados y en lon la longitud de m_vehiculos
-/*Descripcion: Imprime en pantalla un formulario para agregar un nuevo vehiculos a m_vehiculos a id.
-Devuelve m_vehiculos y lon actualizados*/
+//Precondicion: m_vehiculos e id inicializados y en lon la longitud de m_vehiculos.
+//Postcondicion: Imprime en pantalla un formulario para agregar un nuevo vehiculos a m_vehiculos a id.
+//Devuelve m_vehiculos y lon actualizados*/
 void agregar_vehiculo(vehiculos **m_vehiculos,int *lon,int id);
 
-//Precondicion: m_vehiculos y matricula inicializados y en lon la longitud de m_vehiculos
-/*Descripcion: Elimina de m_vehiculos el elemento al que le pertenezca el valor en matricula*/
-void borrar_vehiculo(vehiculos **m_vehiculos,int *lon,char *matricula);
+//Precondicion: m_vehiculos y matricula inicializados y en lon la longitud de m_vehiculos.
+//Descripcion: Elimina de m_vehiculos el elemento que le corresponde el valor de indice.
+void borrar_vehiculo(vehiculos **m_vehiculos,int *lon,int indice);
 
-//Precondicion: m_vehiculos y modif inicializados, en lon la longitud de m_vehiculos e -1<indice<longitud
-/*Descripcion: Reemplaza m_vehiculos[indice] con modif*/
-void modificar_vehiculo(vehiculos *m_vehiculos,int lon,int indice,vehiculos modif);
-
-//Busca vehiculo por matricula devuelve su posicion en m_vehiculos y -1 si no se encontro
+//Precondicion: matricula inicializada.
+//Postcondicion: Busca vehiculo por matricula devuelve su posicion en m_vehiculos y -1 si no se encontro.
 int buscar_vehiculo(char *matricula);
 
+//Precondicion: id_Usuario > 0.
+//Postcondicion: Accede al menu de vehiculos de usuarios admin mostrando todos los vehiculos.
 void menu_admin_vehiculos(int id_Usuario);
